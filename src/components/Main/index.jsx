9d0f4d22d36656840ca1a27ff39590e6f6a78447
SreeChakram-004 <cherrytheraider@gmail.com>
2022-06-09 16:23:39 +0530

    counterAdded

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,49 @@
+import React, { useEffect, useState } from 'react'
 import styles from "./styles.module.css";
+import _, { set } from 'lodash';
+import { Button, Card, Divider, Image, Placeholder } from 'semantic-ui-react';
 
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
 	};
+
+	const [loading,setLoading]=useState(false);
+	const [cards,setCards]=useState()
+	const [vote,setVote]=useState(0)
+
+	// const cards =[
+	// 	{
+	// 	  "id": 1,
+	// 	  "dishName": "Lasagne",
+	// 	  "description": "Breaded fried chicken with waffles, and a side of maple syrup.",
+	// 	  "image": "https://loremflickr.com/300/300/food"
+	// 	},
+	// 	{
+	// 	  "id": 2,
+	// 	  "dishName": "Pho",
+	// 	  "description": "Three eggs with cilantro, tomatoes, onions, avocados and melted Emmental cheese. With a side of roasted potatoes, and your choice of toast or croissant.",
+	// 	  "image": "https://loremflickr.com/300/300/food"
+	// 	},
+	//   ]
+
+	  useEffect(()=>{
+		  fetch('https://raw.githubusercontent.com/syook/react-dishpoll/main/db.json')
+		       .then(response =>response.json())
+			   .then(data=>setCards(data))
+
+	  })
+
+
+	 const handleLoadingClick = () => {
+		setLoading(true)
+		setTimeout(() => {
+		  setLoading(false)
+		}, 3000)
+	  }
+
+	
 
 	return (
 		<div className={styles.main_container}>
@@ -14,6 +53,53 @@ const Main = () => {
 					Logout
 				</button>
 			</nav>
+
+			<div>
+        <Divider />
+		{vote}
+			<Card.Group doubling itemsPerRow={3} stackable>
+          {_.map(cards, (card) => (
+            <Card key={card.header}>
+              {loading ? (
+                <Placeholder>
+                  <Placeholder.Image square />
+                </Placeholder>
+              ) : (
+                // <Image src={`https://robohash.org/${card.id}?set=set2&size=180x180`} />
+				<Image src={card.image} />
+              )}
+
+              <Card.Content>
+                {loading ? (
+                  <Placeholder>
+                    <Placeholder.Header>
+                      <Placeholder.Line length='very short' />
+                      <Placeholder.Line length='medium' />
+                    </Placeholder.Header>
+                    <Placeholder.Paragraph>
+                      <Placeholder.Line length='short' />
+                    </Placeholder.Paragraph>
+                  </Placeholder>
+                ) : (
+                  <>
+                    <Card.Header>{card.header}</Card.Header>
+                    <Card.Meta>{card.dishName}</Card.Meta>
+                    <Card.Description>{card.description}</Card.Description>
+                  </>
+                )}
+              </Card.Content>
+
+              <Card.Content extra>
+                <Button disabled={loading} onClick={()=>card.id===1 ? setVote(vote+1) : setVote(vote+1) } primary>
+                  Vote
+                </Button>
+                <Button disabled={loading} onClick={()=>card.id===1 ? setVote(vote-1) : setVote(vote-1)} >Unvote</Button>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
+			</div>
+
 		</div>
 	);
 };
